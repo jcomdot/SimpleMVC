057ecd3cbb14fccfeffa53b2cae85ce87d833f81
Joonsong Jang <jcomdot@gmail.com>
2014-01-08 13:59:38 +0900

    RowMapper class object variable.

--- src/main/java/com/jcomdot/simplemvc/ActorDao.java
+++ src/main/java/com/jcomdot/simplemvc/ActorDao.java
@@ -16,57 +16,46 @@ public class ActorDao {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 	
-	public void add(final Actor actor) throws ClassNotFoundException, SQLException {
+	private RowMapper<Actor> rowMapper = 
+		new RowMapper<Actor>() {
+			public Actor mapRow(ResultSet rs, int rowNum) throws SQLException {
+				Actor actor = new Actor();
+				actor.setActorId(rs.getInt("actor_id"));
+				actor.setFirstName(rs.getString("first_name"));
+				actor.setLastName(rs.getString("last_name"));
+				actor.setLastUpdate(rs.getTimestamp("last_update"));
+				return actor;
+			}
+		};
+	
+	public void add(final Actor actor) {
 		this.jdbcTemplate.update("insert into actor(first_name, last_name, last_update) values(?, ?, localtimestamp)",
 			actor.getFirstName(), actor.getLastName());
 	}
 	
-	public int getLastIdx() throws ClassNotFoundException, SQLException {
+	public int getLastIdx() {
 		return this.jdbcTemplate.queryForObject("select max(actor_id) as last_value from actor", Integer.class);
 	}
 	
-	public Actor get(int id) throws SQLException {
-		
-		return this.jdbcTemplate.queryForObject("select * from actor where actor_id = ?", new Object[] {id},
-			new RowMapper<Actor>() {
-				public Actor mapRow(ResultSet rs, int rowNum) throws SQLException {
-					Actor actor = new Actor();
-					actor.setActorId(rs.getInt("actor_id"));
-					actor.setFirstName(rs.getString("first_name"));
-					actor.setLastName(rs.getString("last_name"));
-					actor.setLastUpdate(rs.getTimestamp("last_update"));
-					return actor;
-				}
-			}
-		);
+	public Actor get(int id) {
+		return this.jdbcTemplate.queryForObject("select * from actor where actor_id = ?", new Object[] {id}, this.rowMapper);
 	}
 	
-	public List<Actor> getAll() throws SQLException {
-		return this.jdbcTemplate.query("select * from actor order by actor_id desc",
-			new RowMapper<Actor>() {
-				public Actor mapRow(ResultSet rs, int rowNum) throws SQLException {
-					Actor actor = new Actor();
-					actor.setActorId(rs.getInt("actor_id"));
-					actor.setFirstName(rs.getString("first_name"));
-					actor.setLastName(rs.getString("last_name"));
-					actor.setLastUpdate(rs.getTimestamp("last_update"));
-					return actor;
-				}
-			}
-		);
+	public List<Actor> getAll() {
+		return this.jdbcTemplate.query("select * from actor order by actor_id desc", this.rowMapper);
 				
 	}
 	
-	public void deleteAddedRecords() throws SQLException {
+	public void deleteAddedRecords() {
 		this.jdbcTemplate.update("delete from actor where actor_id > 200");
 		this.resetCount();
 	}
 	
-	public void resetCount() throws SQLException {
+	public void resetCount() {
 		this.jdbcTemplate.execute("select setval('public.actor_actor_id_seq', 200, true)");
 	}
 	
-	public int getCount() throws SQLException {
+	public int getCount() {
 		return this.jdbcTemplate.queryForObject("select count(*) from actor", Integer.class);
 	}
 
