ba0c5dbd2dca9628bdd4a42c757f6d6ad2bfc65c
Joonsong <Joonsong@192.168.1.105>
2014-02-21 10:42:15 +0900

    ProxyFactoryBean Test. (1)

--- src/test/java/com/jcomdot/simplemvc/jdk/DynamicProxyTest.java
+++ src/test/java/com/jcomdot/simplemvc/jdk/DynamicProxyTest.java
@@ -10,7 +10,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class ProxyTest {
+public class DynamicProxyTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
