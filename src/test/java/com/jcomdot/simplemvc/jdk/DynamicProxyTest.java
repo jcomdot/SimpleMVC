ce644dabe63604bc30c10f6b89460eec9a33b083
Joonsong <Joonsong@192.168.1.105>
2014-02-22 19:02:13 +0900

    ProxyFactoryBean. (2)

--- src/test/java/com/jcomdot/simplemvc/jdk/DynamicProxyTest.java
+++ src/test/java/com/jcomdot/simplemvc/jdk/DynamicProxyTest.java
@@ -5,49 +5,54 @@ import static org.junit.Assert.*;
 
 import java.lang.reflect.Proxy;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 import org.junit.Test;
+import org.springframework.aop.framework.ProxyFactoryBean;
 
 public class DynamicProxyTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
 	public void simpleProxy() {
-		Hello hello = new HelloTarget();
-		assertThat(hello.sayHello("Tobbung"), is("Hello Tobbung"));
-		assertThat(hello.sayHi("Tobbung"), is("Hi Tobbung"));
-		assertThat(hello.sayThankYou("Tobbung"), is("Thank You Tobbung"));
-	}
-	
-	@Test
-	public void helloUpperProxy() {
-		Hello proxyHello = new HelloUppercase(new HelloTarget());
+		Hello proxyHello = (Hello)Proxy.newProxyInstance(
+				getClass().getClassLoader(),
+				new Class[] { Hello.class },
+				new UppercaseHandler(new HelloTarget()));
 		assertThat(proxyHello.sayHello("Tobbung"), is("HELLO TOBBUNG"));
 		assertThat(proxyHello.sayHi("Tobbung"), is("HI TOBBUNG"));
 		assertThat(proxyHello.sayThankYou("Tobbung"), is("THANK YOU TOBBUNG"));
 	}
-
+	
 	@Test
-	public void helloUpperDynamicProxy() {
-		Hello proxiedHello = (Hello)Proxy.newProxyInstance(
-				getClass().getClassLoader(),
-				new Class[] { Hello.class },
-				new UppercaseHandler(new HelloTarget()));
-		assertThat(proxiedHello.sayHello("Tobbung"), is("HELLO TOBBUNG"));
-		assertThat(proxiedHello.sayHi("Tobbung"), is("HI TOBBUNG"));
-		assertThat(proxiedHello.sayThankYou("Tobbung"), is("THANK YOU TOBBUNG"));
+	public void proxyFactoryBean() {
+		ProxyFactoryBean pfBean = new ProxyFactoryBean();
+		pfBean.setTarget(new HelloTarget());
+		pfBean.addAdvice(new UppercaseAdvice());
+		
+		Hello proxiedHello = (Hello)pfBean.getObject();
+		assertThat(proxiedHello.sayHello("Tobbung"), is("Hello Tobbung"));
+		assertThat(proxiedHello.sayHi("Tobbung"), is("Hi Tobbung"));
+		assertThat(proxiedHello.sayThankYou("Tobbung"), is("Thank You Tobbung"));
 	}
+
+	public class UppercaseAdvice implements MethodInterceptor {
+		@Override
+		public Object invoke(MethodInvocation invocation) throws Throwable {
+			String ret = (String)invocation.proceed();
+			return ret;
+		}
+	}
+	
+	static interface Hello {
+		String sayHello(String name);
+		String sayHi(String name);
+		String sayThankYou(String name);
+	}
+	
+	static class HelloTarget implements Hello {
+		public String sayHello(String name) { return "Hello " + name; }
+		public String sayHi(String name) { return "Hi " + name; }
+		public String sayThankYou(String name) { return "Thank You " + name; }
+	}
+
 }
---
+++ src/test/java/com/jcomdot/simplemvc/jdk/ProxyTest.java
@@ -0,0 +1,53 @@
+package com.jcomdot.simplemvc.jdk;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Proxy;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ProxyTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void simpleProxy() {
+		Hello hello = new HelloTarget();
+		assertThat(hello.sayHello("Tobbung"), is("Hello Tobbung"));
+		assertThat(hello.sayHi("Tobbung"), is("Hi Tobbung"));
+		assertThat(hello.sayThankYou("Tobbung"), is("Thank You Tobbung"));
+	}
+	
+	@Test
+	public void helloUpperProxy() {
+		Hello proxyHello = new HelloUppercase(new HelloTarget());
+		assertThat(proxyHello.sayHello("Tobbung"), is("HELLO TOBBUNG"));
+		assertThat(proxyHello.sayHi("Tobbung"), is("HI TOBBUNG"));
+		assertThat(proxyHello.sayThankYou("Tobbung"), is("THANK YOU TOBBUNG"));
+	}
+
+	@Test
+	public void helloUpperDynamicProxy() {
+		Hello proxiedHello = (Hello)Proxy.newProxyInstance(
+				getClass().getClassLoader(),
+				new Class[] { Hello.class },
+				new UppercaseHandler(new HelloTarget()));
+		assertThat(proxiedHello.sayHello("Tobbung"), is("HELLO TOBBUNG"));
+		assertThat(proxiedHello.sayHi("Tobbung"), is("HI TOBBUNG"));
+		assertThat(proxiedHello.sayThankYou("Tobbung"), is("THANK YOU TOBBUNG"));
+	}
+}
