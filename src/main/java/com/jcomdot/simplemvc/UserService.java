6560e7a20d98db376e105b42fb3b81da65b18989
Joonsong <Joonsong@192.168.1.105>
2014-03-07 18:12:07 +0900

    Transaction annotation. (3)

--- src/main/java/com/jcomdot/simplemvc/UserService.java
+++ src/main/java/com/jcomdot/simplemvc/UserService.java
@@ -2,11 +2,14 @@ package com.jcomdot.simplemvc;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
 public interface UserService {
 	void add(User user);
 	void update(User user);
-	User get(String id);
-	List<User> getAll();
+	@Transactional(readOnly=true) User get(String id);
+	@Transactional(readOnly=true) List<User> getAll();
 	void deleteAll();
 	
 	void upgradeLevels();
