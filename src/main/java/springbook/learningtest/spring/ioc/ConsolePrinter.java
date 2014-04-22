98e2f2318c59defcbd4d82428d07884bf707433b
Joonsong Jang <jsjang@jcomdot.com>
2014-04-22 18:24:50 +0900

    Vol 2. ch1. IoC Container & DI. p55.

---
+++ src/main/java/springbook/learningtest/spring/ioc/ConsolePrinter.java
@@ -0,0 +1,10 @@
+package springbook.learningtest.spring.ioc;
+
+public class ConsolePrinter implements Printer {
+
+	@Override
+	public void print(String message) {
+		System.out.println(message);
+	}
+
+}
