aa773227ba382b50251d80cbb322d6e38697852f
Joonsong Jang <jsjang@jcomdot.com>
2014-04-23 17:58:18 +0900

    Vol 2. ch1. IoC Container & DI. p59.

---
+++ src/main/java/springbook/learningtest/spring/ioc/RegHelloBeanTest.java
@@ -0,0 +1,59 @@
+package springbook.learningtest.spring.ioc;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.RuntimeBeanReference;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.context.support.StaticApplicationContext;
+
+public class RegHelloBeanTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		StaticApplicationContext ac = new StaticApplicationContext();
+		ac.registerSingleton("hello1", Hello.class);
+		
+		BeanDefinition helloDef = new RootBeanDefinition(Hello.class);
+		helloDef.getPropertyValues().addPropertyValue("name", "Spring");
+		ac.registerBeanDefinition("hello2", helloDef);
+		
+		Hello hello1 = ac.getBean("hello1", Hello.class);
+		assertThat(hello1, is(notNullValue()));
+		
+		Hello hello2 = ac.getBean("hello2", Hello.class);
+		assertThat(hello2.sayHello(), is("Hello Spring"));
+		assertThat(hello1, is(not(hello2)));
+		assertThat(ac.getBeanFactory().getBeanDefinitionCount(), is(2));
+		
+		ac.close();
+	}
+	
+	@Test
+	public void registerBeanWithDependency() {
+		StaticApplicationContext ac = new StaticApplicationContext();
+		
+		ac.registerBeanDefinition("printer", new RootBeanDefinition(StringPrinter.class));
+		
+		BeanDefinition helloDef = new RootBeanDefinition(Hello.class);
+		helloDef.getPropertyValues().addPropertyValue("name", "Spring");
+		helloDef.getPropertyValues().addPropertyValue("printer", new RuntimeBeanReference("printer"));
+		
+		ac.registerBeanDefinition("hello", helloDef);
+		
+		Hello hello = ac.getBean("hello", Hello.class);
+		hello.print();
+		
+		assertThat(ac.getBean("printer").toString(), is("Hello Spring"));
+		
+		ac.close();
+	}
+
+}
