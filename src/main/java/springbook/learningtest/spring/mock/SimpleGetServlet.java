313f37985a8b77a4ffef92ee4554b901d033ca88
Joonsong Jang <jsjang@jcomdot.com>
2014-05-30 11:29:24 +0900

    Vol 2. ch3. Spring MVC 	- 3.2.2 Spring web learningtest.

---
+++ src/main/java/springbook/learningtest/spring/mock/SimpleGetServlet.java
@@ -0,0 +1,23 @@
+package springbook.learningtest.spring.mock;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SimpleGetServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -8966138227444750818L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("name");
+		
+		resp.getWriter().print("<HTML><BODY>");
+		resp.getWriter().print("Hello " + name);
+		resp.getWriter().print("</BODY></HTML>");
+	}
+	
+}
