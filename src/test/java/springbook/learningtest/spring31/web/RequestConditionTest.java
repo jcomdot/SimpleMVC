9e6c6b4fe068cd122013217b6e2ccbcf7e00adfb
Joonsong Jang <jsjang@jcomdot.com>
2014-06-27 17:34:10 +0900

    Vol 2. ch4. Spring @MVC 	- 4.9.2 RequestMappingHandlerMapping. (2)

--- src/test/java/springbook/learningtest/spring31/web/RequestConditionTest.java
+++ src/test/java/springbook/learningtest/spring31/web/RequestConditionTest.java
@@ -1,4 +1,5 @@
 package springbook.learningtest.spring31.web;
+
 import java.util.Map;
 
 import javax.servlet.ServletException;
@@ -8,50 +9,119 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
-
 public class RequestConditionTest extends AbstractDispatcherServlet31Test {
-	
+
 	@Test
 	public void patternRC() throws ServletException {
-		setClasses(AppConfig.class, PatternController.class, ParamsController.class);
+		setClasses(AppConfig.class, PatternController.class,
+				ParamsController.class, HeadersController.class,
+				ConsumesController.class, ProducesController.class,
+				RequestMethodController.class, Quiz4.class, Quiz5.class,
+				Quiz6.class);
 		buildDispatcherServlet();
-		
+
 		RequestMappingHandlerMapping rmhm = getBean(AppConfig.class).rmhm;
-		for (Map.Entry<RequestMappingInfo, HandlerMethod> e : rmhm.getHandlerMethods().entrySet()) {
+		for (Map.Entry<RequestMappingInfo, HandlerMethod> e : rmhm
+				.getHandlerMethods().entrySet()) {
 			System.out.println(e.getKey() + " => " + e.getValue());
 		}
 	}
 
 	@Configuration
 	static class AppConfig extends WebMvcConfigurationSupport {
-		@Autowired RequestMappingHandlerMapping rmhm;
-		
+		@Autowired
+		RequestMappingHandlerMapping rmhm;
+
 		@Override
 		public RequestMappingHandlerMapping requestMappingHandlerMapping() {
 			RequestMappingHandlerMapping rmhm = new RequestMappingHandlerMapping();
 			return rmhm;
 		}
 	}
+
 	@Controller
-	@RequestMapping({"/a", "/b"})
+	@RequestMapping({ "/a", "/b" })
 	static class PatternController {
-		@RequestMapping({"/c", "/d"})
+		@RequestMapping({ "/c", "/d" })
 		public String hello() {
 			return "hello.jsp";
 		}
 	}
+
 	@Controller
-	@RequestMapping(params={"p1", "p2"})
+	@RequestMapping(params = { "p1", "p2" })
 	static class ParamsController {
-		@RequestMapping(params={"p3", "p4"})
+		@RequestMapping(params = { "p3", "p4" })
 		public String hello2() {
 			return "hello.jsp";
 		}
 	}
 
+	@Controller
+	@RequestMapping(headers = { "h1", "h2" })
+	static class HeadersController {
+		@RequestMapping(value = "/f", headers = { "h3", "h4" })
+		public String hello3() {
+			return "hello.jsp";
+		}
+	}
+
+	@Controller
+	@RequestMapping
+	static class ConsumesController {
+		@RequestMapping(value = "/g", consumes = { "c3/text", "c4/json" })
+		public String hello4() {
+			return "hello.jsp";
+		}
+	}
+
+	@Controller
+	@RequestMapping(produces = { "r1/text", "r2/text" })
+	static class ProducesController {
+		@RequestMapping(value = "/g", produces = { "r3/text", "r4/json" })
+		public String hello5() {
+			return "hello.jsp";
+		}
+	}
+
+	@Controller
+	@RequestMapping(method = { RequestMethod.GET, RequestMethod.POST })
+	static class RequestMethodController {
+		@RequestMapping(method = { RequestMethod.PUT, RequestMethod.DELETE })
+		public String hello6() {
+			return "hello.jsp";
+		}
+	}
+
+	@Controller
+	@RequestMapping(headers = { "a", "Content-Type=application/json",
+			"Content-Type=multipart/form-data" })
+	public static class Quiz4 {
+		@RequestMapping(headers = { "c", "d" })
+		public void hello() {
+		}
+	}
+
+	@Controller
+	@RequestMapping(headers = { "a", "b" })
+	public static class Quiz5 {
+		@RequestMapping(headers = { "c", "d", "Content-Type=application/json" }, consumes = "multipart/form-data")
+		public void hello() {
+		}
+	}
+
+	@Controller
+	@RequestMapping(consumes = { "application/xml",
+			"application/x-www-form-urlencoded" })
+	public static class Quiz6 {
+		@RequestMapping(consumes = { "multipart/form-data", "application/json" })
+		public void hello() {
+		}
+	}
 }
